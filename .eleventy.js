f2c32514c6422b59e32a6f6df2bd6da1c33ddaa4
Matt O'Leary <mattosurf@gmail.com>
2020-01-19 10:47:20 -0800

    changes

--- .eleventy.js
+++ .eleventy.js
@@ -11,4 +11,13 @@ module.exports = function(eleventyConfig) {
         include: "includes",
     }
     }
+
   }
+
+// newFunction();
+//
+// function newFunction() {
+ //   let today = new Date().toISOString().substr(0, 10);
+  //  document.querySelector("#today").value = today;
+   // document.querySelector("#today2").valueAsDate = new Date();
+// }
