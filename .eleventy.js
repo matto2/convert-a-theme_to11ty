21e8a600a311eef0b609bbc2510c469290c28159
Matt O'Leary <mattosurf@gmail.com>
2020-01-18 20:13:08 -0800

    first commit

---
+++ .eleventy.js
@@ -0,0 +1,14 @@
+module.exports = function(eleventyConfig) {
+    
+    eleventyConfig.addPassthroughCopy("assets")
+
+
+    return {
+      passthroughFileCopy: true,  
+      dir: {
+        input: "src",
+        output: "_site",
+        include: "includes",
+    }
+    }
+  }
