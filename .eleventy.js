af44ca80305404ca350a640ba7c307df6e9da2b0
Matt O'Leary <mattosurf@gmail.com>
2020-01-20 11:20:54 -0800

    changes

--- .eleventy.js
+++ .eleventy.js
@@ -3,7 +3,8 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("assets")
 
     eleventyConfig.addPassthroughCopy("apple-touch-icon.png")
-    
+
+
     return {
       passthroughFileCopy: true,  
       dir: {
